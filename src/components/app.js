94803c021d31f30ec639d07990f42a84a6f5ebcd
Nicole King <kingnt78@yahoo.com>
2017-11-10 14:24:11 -0500

    second commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <p> Hello, I got text </p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducer,
+    applyMiddleware(
+        // thunk works behind the scenes to allow us to make async actions. Without
+        // it, the code in actions/index.js would not work.
+        thunkMiddleware,
+        createLogger() // just to help us see what's going on
+    )
+);
+
+// store.dispatch(()); needs parameters
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
